dae23af9dacc5973888099e1f026ee5393684359
salim-dev <salim.elmoussaoui.afpa2017@gmail.com>
2017-03-16 08:52:01 +0100

    create air afpa project

---
+++ src/airafpa/AirAfpa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airafpa;
+
+/**
+ *
+ * @author Salim El Moussaoui <salim.elmoussaoui.afpa2017@gmail.com>
+ */
+public class AirAfpa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
