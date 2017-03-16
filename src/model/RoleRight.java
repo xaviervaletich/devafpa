dc16bdaba8a4a374e4d578fe6e559e13287ea22a
El Moussaoui Salim <elmoussaoui.salim.dev@gmail.com>
2017-03-16 23:19:27 +0100

    change type int => long

--- src/model/RoleRight.java
+++ src/model/RoleRight.java
@@ -10,17 +10,17 @@ package model;
  * @author Salim El Moussaoui <salim.elmoussaoui.afpa2017@gmail.com>
  */
 public class RoleRight {
-    private int user_id;
+    private long user_id;
     private boolean admin;
     private boolean blocked;
 
-    public RoleRight(int user_id, boolean admin, boolean blocked) {
+    public RoleRight(long user_id, boolean admin, boolean blocked) {
         this.user_id = user_id;
         this.admin = admin;
         this.blocked = blocked;
     }
 
-    public int getUser_id() {
+    public long getUser_id() {
         return user_id;
     }
 
@@ -32,7 +32,7 @@ public class RoleRight {
         return blocked;
     }
 
-    public void setUser_id(int user_id) {
+    public void setUser_id(long user_id) {
         this.user_id = user_id;
     }
 
@@ -42,6 +42,44 @@ public class RoleRight {
 
     public void setBlocked(boolean blocked) {
         this.blocked = blocked;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleRight{" + "user_id=" + user_id + ", admin=" + admin + ", blocked=" + blocked + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + (int) (this.user_id ^ (this.user_id >>> 32));
+        hash = 73 * hash + (this.admin ? 1 : 0);
+        hash = 73 * hash + (this.blocked ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RoleRight other = (RoleRight) obj;
+        if (this.user_id != other.user_id) {
+            return false;
+        }
+        if (this.admin != other.admin) {
+            return false;
+        }
+        if (this.blocked != other.blocked) {
+            return false;
+        }
+        return true;
     }
     
 }
