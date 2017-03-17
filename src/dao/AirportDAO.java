71fe554c6a0d45a8ea2d71950c1dc7784dde0d3b
salim-dev <salim.elmoussaoui.afpa2017@gmail.com>
2017-03-17 10:36:02 +0100

    add comments

--- src/dao/AirportDAO.java
+++ src/dao/AirportDAO.java
@@ -22,10 +22,18 @@ public class AirportDAO extends DAO<Airport, String> {
         super();
     }
 
+    /**
+     *
+     * Create airport
+     *
+     * @param airport
+     * @return airport object
+     */
     @Override
     public Airport create(Airport airport) {
-
+        // create aiport empty
         Airport airportCreate = new Airport();
+        //check if connect db
         if (this.bddmanager.connect()) {
 
             try {
@@ -38,11 +46,12 @@ public class AirportDAO extends DAO<Airport, String> {
                 pst.setString(1, airport.getAita());
                 pst.setString(2, airport.getCity());
                 pst.setString(3, airport.getCountry());
-                // excute insert row in table
+                // excute update row in table
                 int insert = pst.executeUpdate();
-                // if insert in table 
+                // if update in table 
                 if (insert != 0) {
-                    airportCreate = airport;            
+                    // assign airport
+                    airportCreate = airport;
                 }
             } catch (SQLException ex) {
                 ex.printStackTrace();
@@ -52,14 +61,20 @@ public class AirportDAO extends DAO<Airport, String> {
         } else {
             return airportCreate;
         }
- 
+
         return airportCreate;
     }
 
+    /**
+     * Update airport
+     *
+     * @param airport
+     * @return true is update airport, false isn't update
+     */
     @Override
     public boolean update(Airport airport) {
         boolean success = false;
-
+        //check if connect db
         if (this.bddmanager.connect()) {
 
             try {
@@ -72,9 +87,9 @@ public class AirportDAO extends DAO<Airport, String> {
                 pst.setString(1, airport.getCity());
                 pst.setString(2, airport.getCountry());
                 pst.setString(3, airport.getAita());
-                // excute update row in table
+                // excute delete row in table
                 int insert = pst.executeUpdate();
-                // if insert in table 
+                // if delete in table 
                 if (insert != 0) {
                     success = true;
                 }
@@ -89,10 +104,16 @@ public class AirportDAO extends DAO<Airport, String> {
         return success;
     }
 
+    /**
+     * delete airport
+     *
+     * @param primary_key
+     * @return true is delete airport, false isn't delete
+     */
     @Override
     public boolean delete(String primary_key) {
         boolean success = false;
-
+        //check if connect db
         if (this.bddmanager.connect()) {
 
             try {
@@ -121,22 +142,31 @@ public class AirportDAO extends DAO<Airport, String> {
 
     }
 
+    /**
+     * get all airports
+     *
+     * @return all airports
+     */
     @Override
     public ArrayList getAll() {
+        // create array airport empty
         ArrayList<Airport> listAirport = new ArrayList<>();
+        //check if connect db
         if (this.bddmanager.connect()) {
 
             try {
+                // create requete 
                 Statement st = this.bddmanager
-                                .getConnectionManager()
-                                .createStatement();
+                        .getConnectionManager()
+                        .createStatement();
                 String requete = "SELECT * FROM airports";
+                // excute requete
                 ResultSet rs = st.executeQuery(requete);
-
+                // insert all airports in array object airport
                 while (rs.next()) {
                     Airport el = new Airport(
-                            rs.getString("aita"), 
-                            rs.getString("city"), 
+                            rs.getString("aita"),
+                            rs.getString("city"),
                             rs.getString("country")
                     );
                     listAirport.add(el);
@@ -155,16 +185,29 @@ public class AirportDAO extends DAO<Airport, String> {
         return listAirport;
     }
 
+    /**
+     * find airport
+     *
+     * @param primary_key
+     * @return airport
+     */
     @Override
     public Airport find(String primary_key) {
+        // create array airport empty
         Airport airport = new Airport();
+        //check if connect db
         if (this.bddmanager.connect()) {
 
             try {
+                // create statement for find 
                 Statement st = this.bddmanager.getConnectionManager().createStatement();
+                // create requete add primary key
                 String requete = "SELECT * FROM airports WHERE aita LIKE \"" + primary_key + "\"";
+                // excute requete
                 ResultSet rs = st.executeQuery(requete);
-                if(rs.next()){
+                // if result is full
+                if (rs.next()) {
+                    // insert airports in object
                     airport.setAita(rs.getString("aita"));
                     airport.setCity(rs.getString("city"));
                     airport.setCountry(rs.getString("country"));
@@ -181,27 +224,27 @@ public class AirportDAO extends DAO<Airport, String> {
 
         return airport;
     }
+
     /**
-     * il verifie si l'object airport est rempli ou vide
+     * It checks if the object airport is filled or empty
+     *
      * @param airport
-     * @return false is empty and true is full 
+     * @return false is empty and true is full
      */
-    public boolean isValid(Airport airport){
+    public boolean isValid(Airport airport) {
         boolean isValid = true;
-        
+
         // if code aita is empty or null
-        if(airport.getAita().isEmpty() || airport.getAita() == null){
+        if (airport.getAita().isEmpty() || airport.getAita() == null) {
+            isValid = false;
+        } // if city is empty or null
+        else if (airport.getCity().isEmpty() || airport.getCity() == null) {
+            isValid = false;
+        } // if country is empty or null
+        else if (airport.getCountry().isEmpty() || airport.getCountry() == null) {
             isValid = false;
         }
-        // if city is empty or null
-        else if(airport.getCity().isEmpty() || airport.getCity() == null){
-            isValid = false;
-        }
-       // if country is empty or null
-        else if(airport.getCountry().isEmpty() || airport.getCountry() == null){
-            isValid = false;
-        }        
-        return isValid;            
+        return isValid;
     }
 
 }
