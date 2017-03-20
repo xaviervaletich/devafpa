7895beb45320ec329180734e9d2566fdb0554cb0
salim-dev <salim.elmoussaoui.afpa2017@gmail.com>
2017-03-20 13:40:35 +0100

    update

--- src/model/User.java
+++ src/model/User.java
@@ -5,6 +5,8 @@
  */
 package model;
 
+import java.util.Objects;
+
 /**
  *
  * @author Salim El Moussaoui <salim.elmoussaoui.afpa2017@gmail.com>
@@ -14,18 +16,29 @@ public class User {
     private long id;
     private String firstname;
     private String lastname;
-    private String adress;
+    private String address;
     private String city;
     private String country;
     private String tel;
     private String mail;
     private int function;
-
-    public User(long id, String firstname, String lastname, String adress, String city, String country, String tel, String mail, int function) {
+    
+    public User() {
+        this.id = 0;
+        this.firstname = "";
+        this.lastname = "";
+        this.address = "";
+        this.city = "";
+        this.country = "";
+        this.tel = "";
+        this.mail = "";
+        this.function = 0;
+    }
+    public User(long id, String firstname, String lastname, String address, String city, String country, String tel, String mail, int function) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.adress = adress;
+        this.address = address;
         this.city = city;
         this.country = country;
         this.tel = tel;
@@ -45,8 +58,8 @@ public class User {
         return lastname;
     }
 
-    public String getAdress() {
-        return adress;
+    public String getAddress() {
+        return address;
     }
 
     public String getCity() {
@@ -81,8 +94,8 @@ public class User {
         this.lastname = lastname;
     }
 
-    public void setAdress(String adress) {
-        this.adress = adress;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public void setCity(String city) {
@@ -103,6 +116,68 @@ public class User {
 
     public void setFunction(int function) {
         this.function = function;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", city=" + city + ", country=" + country + ", tel=" + tel + ", mail=" + mail + ", function=" + function + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 79 * hash + Objects.hashCode(this.firstname);
+        hash = 79 * hash + Objects.hashCode(this.lastname);
+        hash = 79 * hash + Objects.hashCode(this.address);
+        hash = 79 * hash + Objects.hashCode(this.city);
+        hash = 79 * hash + Objects.hashCode(this.country);
+        hash = 79 * hash + Objects.hashCode(this.tel);
+        hash = 79 * hash + Objects.hashCode(this.mail);
+        hash = 79 * hash + this.function;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.function != other.function) {
+            return false;
+        }
+        if (!Objects.equals(this.firstname, other.firstname)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastname, other.lastname)) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        if (!Objects.equals(this.country, other.country)) {
+            return false;
+        }
+        if (!Objects.equals(this.tel, other.tel)) {
+            return false;
+        }
+        if (!Objects.equals(this.mail, other.mail)) {
+            return false;
+        }
+        return true;
     }
     
     
